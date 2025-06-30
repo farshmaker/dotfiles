03643f1e375ac0374b0b040d33fafedf0606405f
Yahor Averchanka <killenbhv@gmail.com>
2025-07-01 00:41:26 +0200

    add nvim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,13 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
+require 'core.options'
+require 'core.keymaps'
+require 'core.autocmds'
+require 'plugins'
