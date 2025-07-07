9fe56da38e61ac885b750cd55efa5b6108228567
Yahor Averchanka <killenbhv@gmail.com>
2025-07-07 11:29:01 +0200

    added jk keymap for <Esc>

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -23,3 +23,5 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 vim.keymap.set('n', 'gp', '`[v`]', { desc = 'Select pasted text' })
+
+vim.keymap.set('i', 'jk', '<Esc>', { noremap = true, silent = true })
